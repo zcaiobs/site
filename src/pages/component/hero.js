ea5f97eca91577936c09ac7a5dd2cde6fe05a87e
Caio <caiobernardodasilva@gmail.com>
2020-11-01 00:45:38 -0300

    Task completed

--- src/pages/component/hero.js
+++ src/pages/component/hero.js
@@ -10,7 +10,7 @@ const Hero = (props) => {
             <div className="container">
                 <div>
                     <h1>{props.text}</h1>
-                    <p>Caro visitante, seja bem vindo! Aqui você encontra serviços como: ajuste, conserto, customização e muito mais : ) <br/>Contamos com profissionais qualificados e com muita experiência para lhe atender da melhor maneira.<br/>Solicite já um orçamento!</p>
+                    <p>Caro visitante, seja bem vindo! Aqui você encontra serviços como: ajuste, conserto, customização e muito mais. <br/>Contamos com profissionais qualificados e com muita experiência para lhe atender da melhor maneira.<br/>Solicite já um orçamento!</p>
                     <Button className='btn1' text='Eu quero!'/>
                 </div>
                     <img src={props.img} alt="seam"/>
